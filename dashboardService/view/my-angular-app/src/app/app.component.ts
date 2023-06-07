bf390acb4cabc6cee8b17bcd88012e42bfcf3d9a
wadewilsones <uladaharanina@gmail.com>
2023-06-07 14:38:13 -0400

    Tags display was added

--- dashboardService/view/my-angular-app/src/app/app.component.ts
+++ dashboardService/view/my-angular-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ChangeDetectorRef, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { PostsComponent } from './singlePost/posts.component'
 
@@ -11,14 +11,38 @@ export class AppComponent {
   title = 'my-angular-app';
   @ViewChild(PostsComponent) postsComponent!: PostsComponent;
   @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
+
+  //Elements
+  @ViewChild('tag') tagInput!: ElementRef;
+
   searchTitle: string = " ";
+  submittedTag: string = "";
+  postName: string = '';
 
   //Constructor for dependency injection
   constructor( private changeDetectorRef: ChangeDetectorRef){}
 
-  //Handle search by title
 
-  postName: string = '';
+  //Handle search by tag
+  tagSubmit(event:Event, tagValue: string): void{
+
+    //Change post
+    this.postsComponent.ChangePosts(event, tagValue);
+    //Display tag
+    this.submittedTag = tagValue;
+
+  }
+
+  //Clear tag form if close tag was clicked
+  clearTags(event:Event):void{
+
+    this.submittedTag = "";
+    this.tagInput.nativeElement.value = "";
+    this.postsComponent.getPosts();
+  }
+
+
+  //Handle search by title
 
   handleHeaderSearchSubmit(postName: string){
     this.postName = postName;
